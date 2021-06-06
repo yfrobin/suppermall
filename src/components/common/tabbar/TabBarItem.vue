989927555beb55106a5567ad07f303b24bd848e3
yefei-jxgm <2270868031@qq.com>
2021-06-06 10:06:55 +0800

    'tabbar'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tar-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active" :style="activeStyle"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'deepPink'
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path)
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  }
+}
+</script>
+
+<style>
+
+.tar-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+
+.tar-bar-item img {
+  height: 24px;
+  margin-top: 3px;
+  margin-bottom: 2px;
+  vertical-align: middle;
+}
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/home/Home')
+  },
+  {
+    path: '/category',
+    component: () => import(/* webpackChunkName: "about" */ '../views/category/Category')
+  },
+  {
+    path: '/cart',
+    component: () => import(/* webpackChunkName: "about" */ '../views/cart/Cart')
+  },
+  {
+    path: '/profile',
+    component: () => import(/* webpackChunkName: "about" */ '../views/profile/Profile')
   }
 ]
 
